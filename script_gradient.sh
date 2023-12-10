c0be6f725a8cb2a82a48904f5379431aecf3d45c
TMK04 <floer.learner.01@gmail.com>
2023-12-10 14:32:27 +0800

    refactor(gradient & setup scripts): sync to use other scripts

--- script_gradient.sh
+++ script_gradient.sh
@@ -6,20 +6,21 @@ apt update && apt upgrade -y
 apt-get install build-essential -y
 apt-get install ffmpeg git p7zip-full -y
 
-conda update -n base -c defaults conda -y
-pip install --upgrade pip setuptools wheel
-
 ## Git
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get install git-lfs
 git lfs install
 
-## Node
+## Python
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 20
-nvm use 20
-corepack enable
-corepack prepare pnpm@latest --activate
+conda update -n base -c defaults conda -y
+pip install --upgrade pip setuptools wheel
+
+## Download Large
+
+./script_download_large.sh
+
+## Install deps
+
+./script_install_deps.sh
--- script_setup.sh
+++ script_setup.sh
@@ -1,3 +1,4 @@
 # All-in-one Setup
 
 ./script_download_large.sh
+./script_install_deps.sh
