a51e5927ab4641d34498237e262f65a1c4577d9e
TMK04 <floer.learner.01@gmail.com>
2023-12-10 11:15:30 +0800

    refactor(requirements.txt): split into requirements dir

--- script_download_large.sh
+++ script_download_large.sh
@@ -2,9 +2,12 @@
 
 . .env
 
+pip install -r "$ROOT_DIR/requirements/download_large.txt"
+
 ## CodeFormer
 
 cd "$ROOT_DIR/CodeFormer"
+python basicsr/setup.py develop
 python scripts/download_pretrained_models.py CodeFormer
 
 ## LLM
--- script_install_deps.sh
+++ script_install_deps.sh
@@ -4,7 +4,13 @@
 
 # Python
 
-pip install -r "$ROOT_DIR/requirements.txt"
+pip install -r "$ROOT_DIR/requirements/install_deps.txt"
 
 # Node
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 20
+nvm use 20
+corepack enable
+corepack prepare pnpm@latest --activate
