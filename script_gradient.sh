2043ec6b5fe3d041560dd6f263e8f11784a64941
TMK04 <floer.learner.01@gmail.com>
2024-02-20 12:57:35 +0800

    update

--- script_gradient.sh
+++ script_gradient.sh
@@ -6,7 +6,7 @@
 
 apt update && apt upgrade -y
 apt install build-essential -y
-apt install ffmpeg git p7zip-full -y
+apt install git -y
 
 ## Git
 
@@ -17,26 +17,6 @@ git lfs install
 git pull
 chmod +x $ROOT_DIR/*.sh
 ./script_submodules.sh
-
-## Python
-
-conda update -n base conda -y
-conda update -n base anaconda -y
-pip install --upgrade packaging pip setuptools wheel
-conda install cuda -c nvidia/label/cuda-11.8.0 -y
-
-## Huggingface
-
-. "$SCRIPT_HUGGINGFACE"
-
-## Node
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 20
-nvm use 20
-corepack enable
-corepack prepare pnpm@latest --activate
 
 ## Setup
 
--- script_setup.sh
+++ script_setup.sh
@@ -1,7 +1,26 @@
-# Setup
-
 . .env
 
-# Server
+## Base image
 
-ROOT_DIR="$ROOT_DIR/server" ./server/script_setup.sh
+apt install ffmpeg lsof nano p7zip-full tmux -y
+
+## Python
+
+conda update -n base conda -y
+conda update -n base anaconda -y
+pip install --upgrade packaging pip setuptools wheel
+conda install cuda -c nvidia/label/cuda-11.8.0 -y
+
+## Node
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 20
+nvm use 20
+corepack enable
+corepack prepare pnpm@latest --activate
+
+## Setup
+
+./script_setup.sh
+./script_install_deps.sh
