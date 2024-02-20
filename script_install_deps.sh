d8625bad9093a222ff0846b78e361203febe8955
TMK04 <floer.learner.01@gmail.com>
2024-02-20 13:40:59 +0800

    refactor(node install): move from setup to install_deps

--- script_install_deps.sh
+++ script_install_deps.sh
@@ -9,5 +9,10 @@ ROOT_DIR="$ROOT_DIR/server" ./server/script_install_deps.sh
 # Website 
 
 cd "$ROOT_DIR/website"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 . ~/.nvm/nvm.sh
+nvm install 20
+nvm use 20
+corepack enable
+corepack prepare pnpm@latest --activate
 pnpm i --prod
--- script_setup.sh
+++ script_setup.sh
@@ -1,5 +1,3 @@
-. .env
-
 ## Base image
 
 apt install ffmpeg lsof nano p7zip-full tmux -y
@@ -10,12 +8,3 @@ conda update -n base conda -y
 conda update -n base anaconda -y
 pip install --upgrade packaging pip setuptools wheel
 conda install cuda -c nvidia/label/cuda-11.8.0 -y
-
-## Node
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 20
-nvm use 20
-corepack enable
-corepack prepare pnpm@latest --activate
